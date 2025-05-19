37b9ea477a486915af10e0d162fbdf899888018a
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2025-05-19 14:28:04 -0400

    React

---
+++ React/Primeiros_passos/meu_projeto/src/components/Frase.js
@@ -0,0 +1,8 @@
+function Frase() {
+    return(
+        <div>
+            <h1>Meu Primeiro Componente</h1>
+        </div>
+    )
+}
+export default Frase;
---
+++ React/Primeiros_passos/meu_projeto/src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import Frase from './Frase';
+function HelloWorld() {
+    return (
+        <div>
+            <h1>HelloWorld</h1>
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWorld;
